fea64fe16cbf3b3fe99c58d4a6924e0693387f05
Mukhammed Akhmedov <muxammed.axmedov.07@mail.ru>
2026-09-03 13:43:42 +0400

    fixes

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -22,6 +22,9 @@ import (
 	users_postgres_repository "github.com/Muxammednone/golang-todoapp/internal/features/users/repository/postgres"
 	users_service "github.com/Muxammednone/golang-todoapp/internal/features/users/service"
 	users_transport_http "github.com/Muxammednone/golang-todoapp/internal/features/users/transport/http"
+	web_fs_repository "github.com/Muxammednone/golang-todoapp/internal/features/web/repository/file_system"
+	web_service "github.com/Muxammednone/golang-todoapp/internal/features/web/service"
+	web_transport_http "github.com/Muxammednone/golang-todoapp/internal/features/web/transport/http"
 	"go.uber.org/zap"
 
 	_ "github.com/Muxammednone/golang-todoapp/docs"
@@ -83,6 +86,11 @@ func main() {
 	statisticsService := statistics_service.NewStatisticsService(statistivsRepository)
 	statisticsTransportHTTP := statistics_transport_http.NewStatisticsHTTPHandler(statisticsService)
 
+	logger.Debug("initializing feature", zap.String("feature", "web"))
+	webRepository := web_fs_repository.NewWebRepository()
+	webService := web_service.NewWebService(webRepository)
+	webTransportHTTP := web_transport_http.NewWebHTTPHandler(webService)
+
 	logger.Debug("Initializing HTTP server")
 
 	httpServer := core_http_server.NewHTTPServer(
@@ -104,6 +112,7 @@ func main() {
 		apiVersionRouterV1,
 	)
 
+	httpServer.RegisterRoutes(webTransportHTTP.Routes()...)
 	httpServer.RegisterSwagger()
 
 	if err := httpServer.Run(ctx); err != nil {
